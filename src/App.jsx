78a1735f489f92b5fb6b9e19afbc7432b3dda6a0
OverYander <gavinchen8706@gmail.com>
2026-08-12 15:23:04 -0400

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div> start </div>
+    </>
+  )
+}
+
+export default App
