005d78a0de6828c78b3a9f7b403eca080d3795f4
OverYander <gavinchen8706@gmail.com>
2026-08-26 19:32:56 -0400

    global link and button to submit settings --> button function to go to proper route

--- src/components/game-setting-specific/SettingSubmission.jsx
+++ src/components/game-setting-specific/SettingSubmission.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import allLogo from '../../assets/img/all-logo.svg'
 
 function SettingSubmission() {
+  // created for now to track
+  const [gameNumber, setGameNumber] = useState(0)
+
+  function nextGame() {
+    setGameNumber(prev => prev + 1)
+  }
+
   return (
     <>
-      <div className="col-container items-center h-[90vh] pt-[15vh]">
-        <img src={allLogo} className="region-logo"></img>
-        <input type="submit" value="Submit" class="w-full h-fit bg-dullblue-base border-2 rounded-md border-dullblue-light"></input>
+      <div className="col-container items-center h-[90vh] pt-[15vh] justify-between">
+        <a href={`valorant/global`} className="global-logo m-[10px]">
+          <img src={allLogo} alt="Global Logo"/>
+        </a>
+          
+        <button type="submit" className="w-full h-fit parrallel-container bg-valorant-red p-[5px] pt-[8px] m-[10px]">
+          <span className="font-val text-2xl parrallel-text">
+            Play Game
+          </span>
+        </button>
       </div>
     </> 
   )
